3b5a6a4032cfa465b4b320ac55e674684c04d9e0
Konstantin Bogdanov <kbogdanov1@gmail.com>
2016-03-27 20:41:55 +0300

    sendform variation

--- js/main.js
+++ js/main.js
@@ -63,56 +63,61 @@ $(document).ready(function(){
 
 
    // validate contact form
-    $(function() {
-        $('#contact-form').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true                 
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form."
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact.php",
-                    success: function() {
-                        $('#contact-form :input').attr('disabled', 'disabled');
-                        $('#contact-form').fadeTo( "slow", 0.15, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn();
-                        });
-                    },
-                    error: function() {
-                        $('#contact-form').fadeTo( "slow", 0.15, function() {
-                            $('#error').fadeIn();
-                        });
-                    }
-                });
-            }
-        });
-    });
+    // $(function() {
+    //     $('#contact-form').validate({
+    //         rules: {
+    //             name: {
+    //                 required: true,
+    //                 minlength: 2
+    //             },
+    //             email: {
+    //                 required: true,
+    //                 email: true
+    //             },
+    //             message: {
+    //                 required: true                 
+    //             }
+    //         },
+    //         messages: {
+    //             name: {
+    //                 required: "come on, you have a name don't you?",
+    //                 minlength: "your name must consist of at least 2 characters"
+    //             },
+    //             email: {
+    //                 required: "no email, no message"
+    //             },
+    //             message: {
+    //                 required: "um...yea, you have to write something to send this form."
+    //             }
+    //         },
+    //         submitHandler: function(form) {
+    //             $(form).ajaxSubmit({
+    //                 type:"POST",
+    //                 data: $(form).serialize(),
+    //                 url:"contact.php",
+    //                 success: function() {
+    //                     $('#contact-form :input').attr('disabled', 'disabled');
+    //                     $('#contact-form').fadeTo( "slow", 0.15, function() {
+    //                         $(this).find(':input').attr('disabled', 'disabled');
+    //                         $(this).find('label').css('cursor','default');
+    //                         $('#success').fadeIn();
+    //                     });
+    //                 },
+    //                 error: function() {
+    //                     $('#contact-form').fadeTo( "slow", 0.15, function() {
+    //                         $('#error').fadeIn();
+    //                     });
+    //                 }
+    //             });
+    //         }
+    //     });
+    // });
 
 
 
 });
+
+//@prepros-prepend jquery-1.11.3.js
+//@prepros-prepend jquery.scroolly.js
+//@prepros-prepend smooth-scroll.js
+//@prepros-prepend sendform.js
